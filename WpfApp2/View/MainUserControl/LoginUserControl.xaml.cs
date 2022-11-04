a9a80d825680ca95617d4db46e201a5355daf2bc
Dmitriy Ponomarev <pnomarevd38@mail.ru>
2022-11-05 00:48:30 +0300

    sign in

--- WpfApp2/View/MainUserControl/LoginUserControl.xaml.cs
+++ WpfApp2/View/MainUserControl/LoginUserControl.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WpfApp2.Core;
 
 namespace WpfApp2.View.MainUserControl
 {
@@ -28,6 +29,11 @@ namespace WpfApp2.View.MainUserControl
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             App.Current.Shutdown();
+        }
+
+        private void SignOutBtn_Click(object sender, RoutedEventArgs e)
+        {
+            MyFrame.Frame.Navigate(new LoginUC());
         }
     }
 }
